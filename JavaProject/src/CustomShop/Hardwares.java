85a522025ecc8a98ad61f8e2feaa57d4e153380b
PhumPitch <PhumPitchGithub@gmail.com>
2023-03-02 19:10:51 +0700

    Final Project

--- JavaProject/src/CustomShop/Hardwares.java
+++ JavaProject/src/CustomShop/Hardwares.java
@@ -5,6 +5,17 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.SwingConstants;
+import javax.swing.JCheckBox;
 
 public class Hardwares extends JFrame {
 
@@ -38,6 +49,135 @@ public class Hardwares extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-	}
+		
+		JLabel Hardwares = new JLabel("Hardwares");
+		Hardwares.setFont(new Font("Tahoma", Font.BOLD, 23));
+		Hardwares.setBounds(220, 20, 147, 33);
+		contentPane.add(Hardwares);
+		
+		JLabel lblPickUp = new JLabel("Pickups");
+		lblPickUp.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblPickUp.setBounds(109, 75, 92, 33);
+		contentPane.add(lblPickUp);
+		
+		JComboBox cbPickUp = new JComboBox();
+		cbPickUp.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		cbPickUp.setBounds(250, 75, 189, 33);
+		contentPane.add(cbPickUp);
+		cbPickUp.addItem("S-S-S");
+		cbPickUp.addItem("H-S-S");
+		cbPickUp.addItem("H-S-H");
+		
+		JLabel lblSaddle = new JLabel("Saddle");
+		lblSaddle.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblSaddle.setBounds(109, 118, 92, 33);
+		contentPane.add(lblSaddle);
+		
+		
+		JComboBox cbSaddle = new JComboBox();
+		cbSaddle.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		cbSaddle.setBounds(250, 118, 189, 33);
+		contentPane.add(cbSaddle);
+		cbSaddle.addItem("6-Saddle Tremolo");
+		cbSaddle.addItem("2-Piont Tremolo");
+		cbPickUp.addItem("Floyd Rose Tremolo");
+		
+		JLabel lblTuner = new JLabel("Tuner");
+		lblTuner.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblTuner.setBounds(109, 160, 92, 33);
+		contentPane.add(lblTuner);
+		
+		JComboBox cbTuner = new JComboBox();
+		cbTuner.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		cbTuner.setBounds(250, 160, 189, 33);
+		contentPane.add(cbTuner);
+		cbTuner.addItem("Standard Tuners");
+		cbTuner.addItem("Vintage Locking Tuner");
+		cbTuner.addItem("Deluxe Locking Tuner");
+		
+		JLabel Accessories = new JLabel("Accessories");
+		Accessories.setFont(new Font("Tahoma", Font.BOLD, 23));
+		Accessories.setBounds(220, 212, 147, 33);
+		contentPane.add(Accessories);
+		
+		JLabel lblHandSide = new JLabel("Hand Side");
+		lblHandSide.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblHandSide.setBounds(109, 265, 85, 33);
+		contentPane.add(lblHandSide);
+		
+		JComboBox cbHandSide = new JComboBox();
+		cbHandSide.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		cbHandSide.setBounds(250, 265, 189, 33);
+		contentPane.add(cbHandSide);
+		cbHandSide.addItem("Left Handed");
+		cbHandSide.addItem("Right Handed");
+		
+		JLabel lblPickGuard = new JLabel("Pickguard");
+		lblPickGuard.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblPickGuard.setBounds(109, 309, 85, 33);
+		contentPane.add(lblPickGuard);
+		
+		JComboBox cbPickGuard = new JComboBox();
+		cbPickGuard.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		cbPickGuard.setBounds(250, 309, 189, 33);
+		contentPane.add(cbPickGuard);
+		cbPickGuard.addItem("Modern Style");
+		cbPickGuard.addItem("50's Vintage Style");
+		cbPickGuard.addItem("1-Ply Style");
+		cbPickGuard.addItem("3-Ply Style");
+		
+		JLabel lblPlasticColor = new JLabel("Plastic Color");
+		lblPlasticColor.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblPlasticColor.setBounds(109, 351, 147, 33);
+		contentPane.add(lblPlasticColor);
+		
+		JComboBox cbPlasticColor = new JComboBox();
+		cbPlasticColor.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		cbPlasticColor.setBounds(250, 351, 189, 33);
+		contentPane.add(cbPlasticColor);
+		cbPlasticColor.addItem("White");
+		cbPlasticColor.addItem("Aged White");
+		cbPlasticColor.addItem("Vintage White");
+		cbPlasticColor.addItem("Black");
+		
+		JLabel lblHardwareColor = new JLabel("Hardware Color");
+		lblHardwareColor.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblHardwareColor.setBounds(109, 394, 126, 33);
+		contentPane.add(lblHardwareColor);
+		
+		JComboBox cbHardwareColor = new JComboBox();
+		cbHardwareColor.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		cbHardwareColor.setBounds(250, 394, 189, 33);
+		contentPane.add(cbHardwareColor);
+		cbHardwareColor.addItem("Chrome");
+		cbHardwareColor.addItem("Gold");
+		cbHardwareColor.addItem("Vintage Aged");
+		
+		JButton nextButton = new JButton("Next");
+		nextButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+					new Hardwares().setVisible(true);
+					setVisible(false);
+			}
+		});
+		nextButton.setBounds(585, 477, 89, 23);
+		contentPane.add(nextButton);
+		
+		JButton PrevButton = new JButton("Previous");
+		PrevButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				new WoodworkPart().setVisible(true);
+				setVisible(false);
+			}
+		});
+		PrevButton.setBounds(10, 477, 89, 23);
+		contentPane.add(PrevButton);
+		
+		JCheckBox chckbxHardCase = new JCheckBox("Hard Case");
+		chckbxHardCase.setBounds(250, 434, 97, 23);
+		contentPane.add(chckbxHardCase);
+		
+		
 
+	}
 }
--- JavaProject/src/CustomShop/WoodworkPart.java
+++ JavaProject/src/CustomShop/WoodworkPart.java
@@ -6,6 +6,8 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import javax.swing.JComboBox;
 import javax.swing.JTextField;
@@ -41,111 +43,124 @@ public class WoodworkPart extends JFrame {
 	 */
 	public WoodworkPart() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 700,550);
+		setBounds(100, 100, 700, 550);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblBodyWood = new JLabel("Body Wood");
 		lblBodyWood.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		lblBodyWood.setBounds(109, 69, 92, 33);
+		lblBodyWood.setBounds(109, 81, 92, 33);
 		contentPane.add(lblBodyWood);
-		
+
 		JComboBox cbBody = new JComboBox();
 		cbBody.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		
+
 		cbBody.addItem("Alder");
 		cbBody.addItem("Mahogany");
 		cbBody.addItem("Roaster Pine");
-		
-		cbBody.setBounds(236, 70, 189, 33);
+
+		cbBody.setBounds(236, 81, 189, 33);
 		contentPane.add(cbBody);
-		
-		JLabel lblColorBody = new JLabel("Color Body");
+
+		JLabel lblColorBody = new JLabel("Body Color");
 		lblColorBody.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		lblColorBody.setBounds(109, 112, 92, 33);
+		lblColorBody.setBounds(109, 123, 92, 33);
 		contentPane.add(lblColorBody);
-		
+
 		txtColorBody = new JTextField();
-		txtColorBody.setBounds(236, 112, 189, 33);
+		txtColorBody.setBounds(236, 123, 189, 33);
 		contentPane.add(txtColorBody);
 		txtColorBody.setColumns(10);
-		
+
 		JRadioButton relicRadBtn = new JRadioButton("Relic");
 		relicRadBtn.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		relicRadBtn.setBounds(457, 122, 72, 23);
+		relicRadBtn.setBounds(431, 136, 72, 23);
 		contentPane.add(relicRadBtn);
-		
+
 		JLabel bodyPart = new JLabel("Body Part");
 		bodyPart.setFont(new Font("Tahoma", Font.BOLD, 23));
-		bodyPart.setBounds(212, 20, 147, 33);
+		bodyPart.setBounds(220, 20, 147, 33);
 		contentPane.add(bodyPart);
-		
+
 		JLabel neckPart = new JLabel("Neck Part");
 		neckPart.setFont(new Font("Tahoma", Font.BOLD, 23));
-		neckPart.setBounds(212, 161, 147, 33);
+		neckPart.setBounds(220, 185, 119, 33);
 		contentPane.add(neckPart);
-		
+
 		JLabel lblNeckWood = new JLabel("Neck Wood");
 		lblNeckWood.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		lblNeckWood.setBounds(109, 210, 92, 33);
+		lblNeckWood.setBounds(109, 240, 92, 33);
 		contentPane.add(lblNeckWood);
-		
-		
-		
+
 		JComboBox cbNeck = new JComboBox();
-		
+
 		cbNeck.addItem("Alder");
 		cbNeck.addItem("Rosewood");
-		
+
 		cbNeck.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		cbNeck.setBounds(236, 211, 160, 33);
-		contentPane.add(cbNeck);	
-		
+		cbNeck.setBounds(236, 240, 189, 33);
+		contentPane.add(cbNeck);
+
 		JLabel lblShape = new JLabel("Neck Shape");
 		lblShape.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		lblShape.setBounds(109, 253, 104, 33);
+		lblShape.setBounds(109, 282, 104, 33);
 		contentPane.add(lblShape);
-		
+
 		JLabel lblRadius = new JLabel("Neck Radius");
 		lblRadius.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		lblRadius.setBounds(109, 297, 104, 33);
+		lblRadius.setBounds(109, 324, 104, 33);
 		contentPane.add(lblRadius);
-		
+
 		JComboBox cbNeckShape = new JComboBox();
 		cbNeckShape.addItem("C Shape");
 		cbNeckShape.addItem("D Shape");
 		cbNeckShape.addItem("U Shape");
 		cbNeckShape.addItem("V Shape");
 		cbNeckShape.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		cbNeckShape.setBounds(236, 254, 160, 33);
+		cbNeckShape.setBounds(236, 282, 189, 33);
 		contentPane.add(cbNeckShape);
-		
-		
+
 		JComboBox cbRadius = new JComboBox();
 		cbRadius.addItem("7.25\" radius ");
 		cbRadius.addItem("9.5\" radius ");
 		cbRadius.addItem("12\" radius ");
 		cbRadius.addItem("16\" radius ");
 		cbRadius.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		cbRadius.setBounds(236, 297, 160, 33);
+		cbRadius.setBounds(236, 324, 189, 33);
 		contentPane.add(cbRadius);
-		
+
+		JLabel lblFretType = new JLabel("Fret Type");
+		lblFretType.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblFretType.setBounds(109, 366, 92, 33);
+		contentPane.add(lblFretType);
+
+		JComboBox cbFretType = new JComboBox();
+		cbFretType.setBounds(236, 366, 189, 33);
+		contentPane.add(cbFretType);
+		cbFretType.addItem("Narrow Tall Fret");
+		cbFretType.addItem("Medium Jumbo Fret");
+		cbFretType.addItem("Jumbo Fret");
+		cbFretType.setFont(new Font("Tahoma", Font.PLAIN, 16));
+
 		JButton nextButton = new JButton("Next");
 		nextButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				new Hardwares().setVisible(true);
-				setVisible(false);
+
+				if (txtColorBody.getText().isEmpty()) {
+					JOptionPane.showMessageDialog(null, "Please Enter the Color");
+				} else {
+					new Hardwares().setVisible(true);
+					setVisible(false);
+				}
+
 			}
 		});
-		
-	
-		
-		nextButton.setBounds(572, 427, 89, 23);
+		nextButton.setBounds(567, 462, 89, 23);
 		contentPane.add(nextButton);
-		
-	
+
 	}
+
 }
